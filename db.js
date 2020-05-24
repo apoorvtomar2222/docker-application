bbff7540be2c6da7f7dd7c07c8ab0a61ec28d897
Apoorv Tomar <apoorvtomar2222@github.com>
2020-05-24 20:42:27 +0530

    add basic code structure

---
+++ db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const MONGO_HOSTNAME = '127.0.0.1';
+const MONGO_PORT = '27017';
+const MONGO_DB = 'docker-test';
+
+const url = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
+
+mongoose.connect(url, { useNewUrlParser: true }, (err) => {
+    if (err) {
+        console.log(err)
+    }
+    else {
+        console.log('DB connected successfully!');
+    }
+});
